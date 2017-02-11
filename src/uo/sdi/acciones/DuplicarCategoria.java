698fe9978ef337c6984af4099b73782d00896919
Fernando Freije <damperfreije@hotmail.com>
2017-02-11 18:33:08 +0100

    Hotfix in duplicarCategoriaAction

--- src/uo/sdi/acciones/DuplicarCategoria.java
+++ src/uo/sdi/acciones/DuplicarCategoria.java
@@ -8,7 +8,6 @@ import uo.sdi.business.Services;
 import uo.sdi.business.TaskService;
 import uo.sdi.business.exception.BusinessException;
 import uo.sdi.dto.Category;
-import uo.sdi.dto.util.Cloner;
 import alb.util.log.Log;
 
 public class DuplicarCategoria implements Accion {
@@ -23,17 +22,14 @@ public class DuplicarCategoria implements Accion {
 		HttpSession session = request.getSession();
 		Category category = (Category) session.getAttribute("category");
 		
-		Category duplicate = Cloner.clone(category);
-		duplicate.setName(category.getName()+" - copy");
-		//TODO poner datos en la nueva categoria
 		
 		try {
 			TaskService taskService = Services.getTaskService();
-			taskService.createCategory(duplicate);
+			taskService.duplicateCategory(category.getId());
 		}
 		catch (BusinessException b) {
 			request.setAttribute("error", b.getMessage());
-			Log.debug("Algo ha ocurrido creando la categoria: %s",
+			Log.debug("Algo ha ocurrido duplicando la categoria: %s",
 					b.getMessage());
 			resultado="FRACASO";
 		}

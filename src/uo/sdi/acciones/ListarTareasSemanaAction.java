0aa19c65a72310c19d0df0eb897cf96c3cd59c12
pablogarciamiranda <uo245530@uniovi.es>
2017-02-11 14:40:13 +0100

    Almost every Action created

---
+++ src/uo/sdi/acciones/ListarTareasSemanaAction.java
@@ -0,0 +1,20 @@
+package uo.sdi.acciones;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListarTareasSemanaAction implements Accion {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return getClass().getName();
+	}
+
+}

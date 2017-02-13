5a9352862d0851ae38a4789e09207f24500da2ac
Pablo García Miranda <uo245530@uniovi.es>
2017-02-13 13:26:23 +0100

    Added ListarTareasSemana added

--- src/uo/sdi/acciones/ListarTareasSemanaAction.java
+++ src/uo/sdi/acciones/ListarTareasSemanaAction.java
@@ -1,15 +1,44 @@
 package uo.sdi.acciones;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import uo.sdi.business.Services;
+import uo.sdi.business.TaskService;
+import uo.sdi.business.exception.BusinessException;
+import uo.sdi.dto.Task;
+import uo.sdi.dto.User;
+import alb.util.log.Log;
 
 public class ListarTareasSemanaAction implements Accion {
 
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String resultado="EXITO";
+		HttpSession session=request.getSession();
+		User user = (User) session.getAttribute("user");
+		
+		List<Task> listaTareasSemana;
+		
+		try {
+			TaskService taskService = Services.getTaskService();
+			listaTareasSemana=taskService.findWeekTasksByUserId(Long.valueOf(user.getId()));
+			
+			request.setAttribute("listaTareasSemana", listaTareasSemana);
+			Log.debug("Obtenida lista de tareas de la semana conteniendo [%d] tareas", 
+					listaTareasSemana.size());
+		}
+		catch (BusinessException b) {
+			Log.debug("Algo ha ocurrido obteniendo lista de tareas: %s",
+					b.getMessage());
+			resultado="FRACASO";
+		}
+		return resultado;
 	}
 	
 	@Override
